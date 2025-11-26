1c776e21b524b35a63118104bbb3cb206fd859b7
Amrit Singh <87901504+amritksingh121@users.noreply.github.com>
2025-11-27 00:32:46 +0530

    This is my 2nd Version of Calculator which implements Multiplication and Division as well along with Addition and Subsctration from 1st version

--- calculator.sh
+++ calculator.sh
@@ -3,6 +3,9 @@ read -p "Enter num1 " num1
 read -p "Enter num2 " num2
 (( sum=num1+num2 ))
 (( diff=num1-num2 ))
+(( product=num1*num2 ))
+(( dividend=num1/num2 ))
 echo "Sum of values is $sum"
 echo "Difference of values is $diff"
-
+echo "Product of values is $product"
+echo "Divident of values is $dividend"
